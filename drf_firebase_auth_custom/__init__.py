a3d55297d4c15458c6429aaa51011ed32e963f15
Alisher M <fsdamp@gmail.com>
2022-02-24 17:35:38 +0300

    update

--- drf_firebase_auth_custom/__init__.py
+++ drf_firebase_auth_custom/__init__.py
@@ -1,13 +1,13 @@
 """Metadata for drf-firebase-auth-custom package."""
-__title__ = 'drf-firebase-auth_custom'
+__title__ = 'drf-firebase-auth-custom'
 __version__ = '1.0.0'
 __description__ = (
     'Custom Django Rest Framework authentication backend for '
     'parsing Firebase uid tokens and storing as local users.'
 )
-__url__ = 'https://github.com/zuxriddin99/drf-firebase-auth_custom'
+__url__ = 'https://github.com/zuxriddin99/drf-firebase-auth-custom'
 __author__ = 'original author Gary Burgmann(this is custom version)'
-__author_email__ = 'original author garyburgmann@gmail.com(custom version zuz.828@gmail.com)'
+__author_email__ = 'zux.828@gmail.com)'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 Gary Burgmann'
 
